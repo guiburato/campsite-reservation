89ec0329f2cbb8b3cd3d7e00ff6c6f12f0163171
guiburato <81720136+guiburato@users.noreply.github.com>
2022-10-21 06:47:58 -0300

    first commit

---
+++ src/test/java/Backend/Tech/Challenge/Upgrade/CampsiteReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Tech.Challenge.Upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
